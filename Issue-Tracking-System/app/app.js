f5f4470aa98b5dc23c3db7b025b2b12aff258998
pkanev <pvkanev@gmail.com>
2016-04-11 23:16:34 +0300

    Removed views from original angular seed Added basic bootstrap navbar

--- Issue-Tracking-System/app/app.js
+++ Issue-Tracking-System/app/app.js
@@ -4,8 +4,6 @@
 angular.module('issueTrackingSystem', [
   'ngRoute',
   'angular-loading-bar',
-  'issueTrackingSystem.view1',
-  'issueTrackingSystem.view2',
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
