5647e4f01e0635368db13e5165b714a209595cbc
pkanev <pvkanev@yahoo.com>
2016-04-25 16:37:45 +0300

    1.Added Issue details per project and pagination for issues. 2.Changed the style of the project page

--- Issue-Tracking-System/app/projects/projects-controller.js
+++ Issue-Tracking-System/app/projects/projects-controller.js
@@ -66,7 +66,13 @@
 			'$scope',
 			'$routeParams',
 			'projectServices',
-			function($scope, $routeParams, projectServices) {
+			'PAGE_SIZE',
+			function($scope, $routeParams, projectServices, PAGE_SIZE) {
+				$scope.issueParams = {
+		        	'pageNumber' : 1,
+					'pageSize' : PAGE_SIZE,
+		        };
+
 				projectServices.getProjectById($routeParams.id)
 					.then(function(project) {
 						$scope.project = project;
@@ -77,7 +83,19 @@
 					.then(function(issues) {
 						$scope.projectIssues = issues;
 						console.log(issues);
+					}).then(function() {
+						$scope.loadPagination();						
 					});
+
+				$scope.loadPagination = function() {
+            		$scope.totalIssues = $scope.projectIssues.length;
+                    var start = ($scope.issueParams['pageNumber']-1) * $scope.issueParams['pageSize'];
+                    var end = start + $scope.issueParams['pageSize'];
+                    var issuesPerPage = $scope.projectIssues.slice(start, end);
+            		$scope.issuesPerPage = issuesPerPage;
+            		$scope.allIssues = $scope.projectIssues;
+		            
+		        };
 			}
 		])
 })();
